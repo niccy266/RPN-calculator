0ebfa0c26fdc995f1228bb869c8a27b7701c8d34
niccy266 <46858993+niccy266@users.noreply.github.com>
2021-05-05 11:35:01 +1200

    Update RPNApp.java

--- RPNApp.java
+++ RPNApp.java
@@ -3,9 +3,11 @@ import java.util.*;
 public RPNApp {
   
   Scanner scan;
+  Stack stack;
   
   public static void main(String[] args) {
     scan = new Scanner(System.in);
+    stack = new Stack();
     
     while(scan.hasNext()) {
       nextInput(); 
@@ -14,6 +16,46 @@ public RPNApp {
   }
   
   private static void nextInput() {
+    char in = scan.nextChar();
+    
+    switch(in) {
+        
+      case '+' :
+      case '-' :
+      case 'x' :
+      case '/' :
+        operate(in);
+        break;
+      case '(' :
+        openBracket();
+        break;
+      case ')' :
+        closeBracket();
+        break;
+      default :
+        pushNum(in);
+    }
+  }
+    
+  private static void operate(char in) {
     
   }
+    
+  private static void openBracket(char in) {
+    
+  }
+    
+  private static void closeBracket(char in) {
+    
+  }
+  
+    
+  private static void pushNum(char in) {
+    
+  }
+  
+  private static void error(String message) {
+    
+  }
+  
 }
