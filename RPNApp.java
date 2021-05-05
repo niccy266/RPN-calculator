533e228a9ff966d8cd5e5aae5316a64bb15199a4
niccy266 <46858993+niccy266@users.noreply.github.com>
2021-05-05 14:23:18 +1200

    Added code for bracket operation
    also switch most functions to take a scanner as input so it works more smoothly with the brackets system

--- RPNApp.java
+++ RPNApp.java
@@ -9,21 +9,28 @@ public RPNApp {
     scan = new Scanner(System.in);
     stack = new Stack();
     
-    while(scan.hasNext()) {
-      char in = scan.nextChar();
-
-      interpret(in); 
-    }
     
+    readInput(scan)
   }
   
-  /** Takes an input and does the appropriate operation.
-    * @param in the input to interpret
+  /** Reads to the end of a scanner and interpreting data.
+    * @param s scanner to read from
+    **/
+  public static void readInput(Scanner s) {
+    while(s.hasNext()) {
+      interpret(s); 
+    }
+    //saves memory by closing scanner when they are completed
+    s.close()
+  }
+  
+  /** Takes the next input from a scanner and does the appropriate operation.
+    * @param in the scanner to read from and interpret
     */
-  private static void interpret(char in) {
+  private static void interpret(Scanner s) {
+    char in = s.nextChar();
     
     switch(in) {
-        
       case '+' :
       case '-' :
       case 'x' :
@@ -31,7 +38,7 @@ public RPNApp {
         operate(in);
         break;
       case '(' :
-        openBracket();
+        openBracket(s);
         break;
       case ')' :
         closeBracket();
@@ -40,24 +47,68 @@ public RPNApp {
         pushNum(in);
     }
   }
-    
+  
+  /** Takes the last two numbers on the stack and
+    * performs the specified operation.
+    * @param in the operation to perform
+    **/
   private static void operate(char in) {
     
   }
+  
+  /** Reads until it finds a close bracket and 
+    * repeats the operations a number of times
+    * equal to number on top of the stack.
+    **/
+  private static void openBracket(Scanner s) {
+    //getting the symbols inside the brackets and creating a new scanner for it
+    String inBrackets = extractBrackets(s);
+    Scanner bracketStream;
     
-  private static void openBracket(char in) {
-    
+    //number of times to repeat brackets
+    n = stack.pop();
+    for (int i = 0; i < ) {
+      bracketStream = new Scanner(inBrackets);
+      
+    }
   }
+  
+  private static String extractBrackets(Scanner s) {
+    //string for storing the contents of the brackets
+    StringBuilder inBrackets = new StringBuilder;
+    //num of unclosed brackets
+    int numBrackets = 1; 
     
-  private static void closeBracket(char in) {
+    while(s.hasNext() && numBrackets > 0) {
+      char in = s.nextChar();
+      switch(in) {
+        case ')' :
+          numBrackets--;
+          if(numBrackets == 0) {
+            break;
+          }
+        case '(' :
+          numBrackets++;
+        default:
+          inBrackets.append(in);
+      }
+    }
+    
+    return inBrackets.toString();
+  }
+  
+  private static void closeBracket() {
     
   }
   
-    
+  /** Parses the input to an integer and pushes it to the stack.
+    * @param in input char
+    **/
   private static void pushNum(char in) {
     
   }
   
+  /** Method for printing error messages easily. **/
   private static void error(String message) {
     
   } 
