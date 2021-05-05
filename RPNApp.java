7f2a9ce1fe1eef70f23a01f63ed988f22733a2ff
niccy266 <46858993+niccy266@users.noreply.github.com>
2021-05-05 14:38:53 +1200

    Completed bracket functionality
    Should work pretty well. Add your names to the author tags.

--- RPNApp.java
+++ RPNApp.java
@@ -1,17 +1,28 @@
 import java.util.*;
 
+
+/** 
+  * @author Nicolas Concha
+  * @author 
+  * @author 
+  **/
 public RPNApp {
   
   Scanner scan;
   Stack stack;
   
+  /** Main function, creates stack and reads input from System.in.
+    * Starting the interpreting cycle.
+    * @param args system arguments
+    **/
   public static void main(String[] args) {
     scan = new Scanner(System.in);
     stack = new Stack();
     
     
-    readInput(scan)
+    readInput(scan);
   }
+  
   
   /** Reads to the end of a scanner and interpreting data.
     * @param s scanner to read from
@@ -21,8 +32,9 @@ public RPNApp {
       interpret(s); 
     }
     //saves memory by closing scanner when they are completed
-    s.close()
+    s.close();
   }
+  
   
   /** Takes the next input from a scanner and does the appropriate operation.
     * @param in the scanner to read from and interpret
@@ -68,17 +80,26 @@ public RPNApp {
     //number of times to repeat brackets
     n = stack.pop();
     for (int i = 0; i < ) {
+      //resets the bracket scanner so it can be repeated
       bracketStream = new Scanner(inBrackets);
-      
+      //completes the operations within the brackets
+      readInput(bracketStream);
     }
   }
   
+  /** Finds the instructions within a pair of brackets.
+    * Searches until a closing bracket is found, preserves 
+    * recursive brackets by keeping track of the number of unclosed brackets
+    * @param s the string to read bracket data from
+    * @return string of characters between the brackets
+    **/
   private static String extractBrackets(Scanner s) {
     //string for storing the contents of the brackets
     StringBuilder inBrackets = new StringBuilder;
     //num of unclosed brackets
     int numBrackets = 1; 
     
+    //finds the matching closing bracket
     while(s.hasNext() && numBrackets > 0) {
       char in = s.nextChar();
       switch(in) {
@@ -94,11 +115,19 @@ public RPNApp {
       }
     }
     
+    if (numBrackets > 0) {
+      error("mismatched brackets");
+    }
+    
     return inBrackets.toString();
   }
   
+  
+  /** Raises error if there is a closing parenthesis
+    * without an opening parenthesis preceding it at some point
+    **/
   private static void closeBracket() {
-    
+    error("mismatched brackets");
   }
   
   /** Parses the input to an integer and pushes it to the stack.
